a681a65e3f23a938aa0b2e2b27c22bbe9e1d4330
NeoYCChang <yuchen871126@gmail.com>
2023-11-29 11:26:10 +0800

    [2.0.0.0] 2023/10/04
    1.modify from master[1.0.0.0]
    2.append tcp client

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,7 +1,29 @@
-#include <stdio.h>
+#include <QCoreApplication>
+#include "client.h"
+
 int main(int argc, char *argv[])
 {
-    printf("Hello World!\n");
-    getchar();
-    return 0;
+    QCoreApplication a(argc, argv);
+    Client* client = new Client();
+    if(client->slotEnter() == 0)
+    {
+		cout << "Bye!" << endl;
+        return a.exec();
+    }
+    else
+    {
+        char msg[1024];
+        while(true)
+        {
+			cout << "Please enter some message: ";
+			cin.getline(msg,1024);
+            //scanf("%s",msg);
+            client->slotSend(msg);
+            memset(msg,'\0',1024);
+        }
+
+    }
+
+
+    return a.exec();
 }
